2d9ab298d98113f5bb70676478e7229ec5271d9b
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-03-30 09:45:48 -0400

    Added some javascript (added variables and began form submit function

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
+var cityinput = document.querySelector('#city');
+var userinput = document.querySelector('#city-container');
+var search = document.querySelector('#search');
+var city;
 var APIKey = "2fa299772ce8810c137aa45c20f5b624";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+}
+
+userinput.addEventListener('search', formSubmitHandler);
