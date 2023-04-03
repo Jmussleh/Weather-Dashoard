7945cf431160ea40f7f4d254ce5743cfb05d3983
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-04-02 21:01:51 -0400

    Added a link to jQuery in HTML file. Also added code to pull weather data from the openweather api. Still need to append the weather info together and apply it to my webpage.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,38 @@
-var cityinput = document.querySelector('#city');
-var userinput = document.querySelector('#city-container');
-var search = document.querySelector('#search');
-var city;
-var APIKey = "2fa299772ce8810c137aa45c20f5b624";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var Search = $('#search');
+var City = $('#citySearch');
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-}
+function getWeatherData(location) {
+    const apiKey = "2fa299772ce8810c137aa45c20f5b624";
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+    return fetch(api)
+      .then(response => response.json())
+      .then(data => {
+        const weatherData = {
+          temperature: data.main.temp,
+          condition: data.weather[0].main,
+          location: data.name,
+        };
+        return weatherData;
+      });
+  }
 
-userinput.addEventListener('search', formSubmitHandler);
+  function updateUI(weatherData) {
+    var weatherCard = $("#citycontainer");
+    var temperature = $("<p>");
+    temperature.textContent = `${weatherData.temperature}°C`;
+    var condition = $("<p>");
+    condition.textContent = weatherData.condition;
+    var location = $("#<p>");
+    location.textContent = weatherData.location;
+  }
+
+Search.addEventListener("click", () => {
+  const location = City.value;
+  getWeatherData(location)
+    .then(weatherData => {
+      updateUI(weatherData);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
