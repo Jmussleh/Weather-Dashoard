a63e60d53ffe8e4c7df1395438abf98c1efec2aa
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-04-15 18:14:10 -0400

    Added fetch request for lat/lon data. Added containers for the current weather and 5 day forecast in HTML.

--- assets/script.js
+++ assets/script.js
@@ -12,15 +12,42 @@
         return weatherData;
       });
   }*/
+  var SearchH = [];
   var city = document.getElementById('#citySearch');
   var submit = document.getElementById('#search');
+  var CurrentWeatherCon = document.getElementById('#CurrentWeather');
+  var ForecastCon = document.getElementById('#5day');
+  var SearchHistoryCon = document.getElementById('#city-container');
   var api_key = "2fa299772ce8810c137aa45c20f5b624";
-  var baseURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api_key}`;
-  fetch(baseURL, {cache: 'reload'})
-    .then(function (response) {
-      return response.json();
+  var baseURL = 'https://api.openweathermap.org';
+  
+  dayjs.extend(window.dayjs_plugin_utc);
+  dayjs.extend(window.dayjs_plugin_timezone);
+
+
+  function renderData(city, data) {
+    CurrentWeather(city, data.list[0], data.city.timezone);
+    Forecast(data.list);
+  }
+  function fetchWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+    var apiUrl = `${baseURL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`;
+    
+    fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
     })
     .then(function (data) {
-      console.log(data);
+      renderData(city, data);
+    })
+    .catch(function (err) {
+      console.error(err);
     });
+}
+
+
+  //Make whatever data runs through above function run through again with the lat/lon to get weather data.
+
   
